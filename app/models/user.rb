5d7c161fc3242063087058d03b3a405bddb3d957
akioGitHub <akio.k2020@outlook.jp>
2022-07-21 20:20:04 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday, pfresence: true
+
+end
