7d29e373bcd67e192ea65f24a42c1877bf33e748
akioGitHub <akio.k2020@outlook.jp>
2022-07-22 14:10:52 +0900

    新規登録とログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_KANANAME = /\A[ァ-ヶー－]+\z/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday, pfresence: true
-
+  validates :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  validates :last_kana_name, :first_kana_name, format: { with: VALID_KANANAME, message: 'はカタカナで入力してください'}
 end
