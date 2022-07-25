70d2c99200b0f74f1cd3a2197b9c06a740406ce3
akioGitHub <akio.k2020@outlook.jp>
2022-07-25 15:23:54 +0900

    ログイン状況別の条件分岐設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :edit
   def index
     query = 'SELECT * FROM items ORDER BY id DESC '
     @items = Item.find_by_sql(query)
@@ -27,7 +28,13 @@ class ItemsController < ApplicationController
   end
 
   def update
-
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -35,5 +42,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :image, :detail, :price, :category_id, :quality_id, :postage_id, :prefecture_id,
                                  :term_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    redirect_to action: :index unless user_signed_in? && current_user.id == item.user_id
   end
 end
