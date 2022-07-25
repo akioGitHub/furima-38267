e3add94c3a3eb61ddc2d9de885ff47dadb4d45ec
akioGitHub <akio.k2020@outlook.jp>
2022-07-25 16:27:06 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
   def index
     query = 'SELECT * FROM items ORDER BY id DESC '
@@ -20,16 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
+    @item.new(item_params)
     if @item.save
       redirect_to item_path(@item.id)
     else
@@ -47,5 +45,9 @@ class ItemsController < ApplicationController
   def move_to_index
     item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
