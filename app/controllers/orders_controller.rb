4c210cc76c4695ce546aacc95b0be1a5087ae3c0
akioGitHub <akio.k2020@outlook.jp>
2022-07-26 19:01:17 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,29 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
   end
   def create
+    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
+      Payjp.api_key = "sk_test_1182ef2f41f6adc4b4d3e11d"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_shipping_address.save
-      redirect_to root_path
+      redirect_to root_path    
+    else
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.permit(:post_code, :prefecture_id, :municipalitie, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, order_id: @order_shipping_address.order.id)
+    params.require(:order_shipping_address).permit(:post_code, :prefecture_id, :municipalitie, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,18 +1,18 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipalitie, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   VALID_POST_CODE = /\A\d{3}[-]\d{4}\z/
   VALID_PHONE_NUMBER = /\A\d{10,11}\z/
 
   validates :post_code,:prefecture_id,:municipalitie,:house_number,:phone_number, presence: true
-  validates :price, format: { with: VALID_POST_CODE, message: "は半角で入力してください"}
+  validates :post_code, format: { with: VALID_POST_CODE, message: "は半角で入力してください"}
   validates :phone_number, format: { with: VALID_PHONE_NUMBER, message: "は10桁か11桁の数字で入力してください"}
   validates :phone_number, numericality: true
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Shipping_Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id )
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, municipalitie: municipalitie, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id )
   end
 
 end
