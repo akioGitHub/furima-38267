ceab438fc71796434e7e2f0f0080d14b9ebc9eb4
akioGitHub <akio.k2020@outlook.jp>
2022-08-09 17:18:33 +0900

    sns認証導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,23 +2,37 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_KANANAME = /\A[ァ-ヶー－]+\z/
   VALID_NAME = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   has_one :card, dependent: :destroy
   has_one :address
   has_many :items
   has_many :orders
+  has_many :sns_credentials
 
   validates :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, :birthday, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
   validates :last_kana_name, :first_kana_name, format: { with: VALID_KANANAME, message: 'は全角カタカナで入力してください' }
   validates :last_name, :first_name, format: { with: VALID_NAME, message: 'は全角日本語で入力してください' }
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_074623) do
+ActiveRecord::Schema.define(version: 2022_08_09_033316) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -96,6 +96,15 @@ ActiveRecord::Schema.define(version: 2022_08_08_074623) do
     t.index ["order_id"], name: "index_shipping_addresses_on_order_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -129,4 +138,5 @@ ActiveRecord::Schema.define(version: 2022_08_08_074623) do
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
   add_foreign_key "shipping_addresses", "orders"
+  add_foreign_key "sns_credentials", "users"
 end
