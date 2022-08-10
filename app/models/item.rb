1b763d4aee38ed68aa4e211665fa62915b73c8db
akioGitHub <akio.k2020@outlook.jp>
2022-08-10 17:21:04 +0900

    コメント機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :term
   has_many :item_tag_relations
   has_many :tags, through: :item_tag_relations
+  has_many :comments
 
   belongs_to :user
   has_many_attached :images
